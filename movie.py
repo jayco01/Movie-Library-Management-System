966f808838c987554589522357dd3f5ee3579f0c
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-04-04 11:19:28 -0600

    added setter set_id

--- movie.py
+++ movie.py
@@ -9,34 +9,39 @@ class Movie:
         self.__fine_rate = fine_rate
     
     def get_genre_name(self):
-        if self.genre == "0":
-            self.genre = "Action"
-        if self.genre == "1":
-            self.genre = "Comedy"
-        if self.genre == "2":
-            self.genre = "Drama"
-        if self.genre == "3":
-            self.genre = "Horror"
-        if self.genre == "4":
-            self.genre = "Sci-Fi"
-        if self.genre == "5":
-            self.genre = "Romance"
-        if self.genre == "6":
-            self.genre = "Thriller"
-        if self.genre == "7":
-            self.genre = "Animation"
-        if self.genre == "8":
-            self.genre = "Documentary"
-        if self.genre == "9":
-            self.genre = "Fantasy"
+        if self.__genre == "0":
+            self.__genre = "Action"
+        if self.__genre == "1":
+            self.__genre = "Comedy"
+        if self.__genre == "2":
+            self.__genre = "Drama"
+        if self.__genre == "3":
+            self.__genre = "Horror"
+        if self.__genre == "4":
+            self.__genre = "Sci-Fi"
+        if self.__genre == "5":
+            self.__genre = "Romance"
+        if self.__genre == "6":
+            self.__genre = "Thriller"
+        if self.__genre == "7":
+            self.__genre = "Animation"
+        if self.__genre == "8":
+            self.__genre = "Documentary"
+        if self.__genre == "9":
+            self.__genre = "Fantasy"
   
 
     def get_availability(self):
-        if self.available == "True":
-            self.available = "Available"
+        if self.__available == "True":
+            self.__available = "Available"
         else:
-            self.available = "Rented"
+            self.__available = "Rented"
 
+    def set_id(self, id):
+        if isinstance(id, int) and id.strip() != "":
+            self.__id = id
+        else:
+            print("Error! ID must be a valid integer.")
 
     def borrow_movie(self):
         pass
