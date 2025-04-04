5ca291969802866b32a5d2125480d0de95f86e18
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-04-03 18:52:34 -0600

    added other functions in the movie class

--- movie.py
+++ movie.py
@@ -1,12 +1,27 @@
 class Movie:
     def __init__(self,id,title,director,genre,available,price,fine_rate):
-        self.id = id
-        self.title = title
-        self.director = director
-        self.genre = genre
-        self.available = available
-        self.price = price
-        self.fine_rate = fine_rate
+        self.__id = id
+        self.__title = title
+        self.__director = director
+        self.__genre = genre
+        self.__available = available
+        self.__price = price
+        self.__fine_rate = fine_rate
+    
+    def get_genre_name(self):
+        pass
+
+    def get_availability(self):
+        pass
+
+    def borrow_movie(self):
+        pass
+
+    def return_movie(self):
+        pass
+
+    def __str__(self):
+        pass
 
 
         
