62d25a619de3fb097187e4ae7566835bf27def86
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-04-03 18:47:40 -0600

    defined movie class

---
+++ movie.py
@@ -0,0 +1,12 @@
+class Movie:
+    def __init__(self,id,title,director,genre,available,price,fine_rate):
+        self.id = id
+        self.title = title
+        self.director = director
+        self.genre = genre
+        self.available = available
+        self.price = price
+        self.fine_rate = fine_rate
+
+
+        
